9b11e4247f3a928158e16672b9d2f8f5fa86c0fa
Poornima <poornima101@outlook.com>
2025-04-24 17:47:48 +0530

    second commit for RG

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.26.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "287a72ec-2d14-456f-a437-38140513e983"
+  client_id       = "b33de76a-eba0-4756-b222-288bce49eaee"
+  client_secret   = "FWi8Q~B8O29mb19~WoexzkftPTqMp2LLea6_Sci7"
+  tenant_id       = "0f4ee63f-3379-4f5d-a3a1-586d51a6db3b"
+}
+
+resource "azurerm_resource_group" "RG1" {
+  name     = "DevOps-Test-RG"
+  location = "West Europe"
+}
